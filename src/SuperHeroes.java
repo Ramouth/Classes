a76835201a6af567c86d0ea764ec9b17e58c8aa0
Ramouth <42993992+Ramouth@users.noreply.github.com>
2018-11-13 10:12:59 +0100

    Initial commit

---
+++ src/SuperHeroes.java
@@ -0,0 +1,5 @@
+public class SuperHeroes {
+    public SuperHeroes (String name, String superPower, String weakness, int age, int powerNumber); {
+    SuperHeroes Rachid = new SuperHeroes("Rachid","Telekinesis", "Cheese",30, 9001);
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,49 @@
+public class Variables {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSuperpowers() {
+        return superpowers;
+    }
+
+    public void setSuperpowers(String superpowers) {
+        this.superpowers = superpowers;
+    }
+
+    public String getPlaceOfBirth() {
+        return placeOfBirth;
+    }
+
+    public void setPlaceOfBirth(String placeOfBirth) {
+        this.placeOfBirth = placeOfBirth;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getStrenght() {
+        return strenght;
+    }
+
+    public void setStrenght(int strenght) {
+        this.strenght = strenght;
+    }
+
+    public String name;
+    public String superpowers;
+    public String placeOfBirth;
+    public int age;
+    public int strenght;
+
+
+}
