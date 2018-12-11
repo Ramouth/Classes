a28bbcae8afefa6486007daa165c1a8ff5d242b3
EmilDino <EmilLD123@hotmail.com>
2018-12-11 12:08:28 +0100

    dokomemnteret

--- src/SuperHeroes.java
+++ src/SuperHeroes.java
@@ -1,5 +1,7 @@
 public class SuperHeroes {
+    /** SuperHeroes is not functional*/
     public SuperHeroes (String name, String superPower, String weakness, int age, int powerNumber); {
+        /** adds SuperHeroes object named Rachid */
     SuperHeroes Rachid = new SuperHeroes("Rachid","Telekinesis", "Cheese",30, 9001);
     }
 }
--- src/Variables.java
+++ src/Variables.java
@@ -1,40 +1,51 @@
 public class Variables {
+
+    /** getter for name */
     public String getName() {
         return name;
     }
 
+    /** setter for name */
     public void setName(String name) {
         this.name = name;
     }
 
+    /** getter for superpowers */
     public String getSuperpowers() {
         return superpowers;
     }
 
+    /** setter for superpowers */
     public void setSuperpowers(String superpowers) {
         this.superpowers = superpowers;
     }
 
+    /** getter for placeOfBirth */
     public String getPlaceOfBirth() {
         return placeOfBirth;
     }
 
+    /** setter for placeOfBirth */
     public void setPlaceOfBirth(String placeOfBirth) {
         this.placeOfBirth = placeOfBirth;
     }
 
+    /** getter for age*/
     public int getAge() {
         return age;
     }
 
+    /** setter for age */
     public void setAge(int age) {
         this.age = age;
     }
 
+    /** getter for strenght */
     public int getStrenght() {
         return strenght;
     }
 
+    /** setter strenght */
     public void setStrenght(int strenght) {
         this.strenght = strenght;
     }
